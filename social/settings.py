7da9fa00e278fe6d4f113c477114d9f9c85c7fa8
himanshu282005 <115345975+himanshu282005@users.noreply.github.com>
2025-04-02 11:46:04 +0530

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#h_vp-^3w*^%2vi&3w)712s@cp86g1h#drj(ow_+u%#wr3v1t#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['15.206.151.73', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['52.66.162.118', 'localhost', '0.0.0.0']
 
 
 # Application definition
