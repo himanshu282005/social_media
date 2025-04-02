93f947a10c7f4c228dd70b1e4b84d7e3e58ccc65
himanshu282005 <115345975+himanshu282005@users.noreply.github.com>
2025-04-02 11:27:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
-docker build -t $JOB_NAME:$BUILD_ID .
+#!/bin/bash
+set -e  # Stop script if any command fails
 
-docker tag $JOB_NAME:$BUILD_ID himanshu519/$JOB_NAME:$BUILD_ID
+# Convert JOB_NAME to lowercase to prevent Docker errors
+LOWER_JOB_NAME=$(echo "$JOB_NAME" | tr '[:upper:]' '[:lower:]')
 
-docker tag $JOB_NAME:$BUILD_ID himanshu519/$JOB_NAME:latest
+# Build Docker image
+docker build -t $LOWER_JOB_NAME:$BUILD_ID .
 
-docker push himanshu519/$JOB_NAME:$BUILD_ID
+# Tag Docker image
+docker tag $LOWER_JOB_NAME:$BUILD_ID himanshu519/$LOWER_JOB_NAME:$BUILD_ID
+docker tag $LOWER_JOB_NAME:$BUILD_ID himanshu519/$LOWER_JOB_NAME:latest
 
-docker push himanshu519/$JOB_NAME:latest
+# Push Docker image to DockerHub
+docker push himanshu519/$LOWER_JOB_NAME:$BUILD_ID
+docker push himanshu519/$LOWER_JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID himanshu519/$JOB_NAME:$BUILD_ID himanshu519/$JOB_NAME:latest
+# Remove local images to free space
+docker rmi -f $LOWER_JOB_NAME:$BUILD_ID \
+              himanshu519/$LOWER_JOB_NAME:$BUILD_ID \
+              himanshu519/$LOWER_JOB_NAME:latest
